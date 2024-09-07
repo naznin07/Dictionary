028b6e32f81becd6f608b27385daa0ebde1b8141
naznin07 <naffect1@gmail.com>
2024-09-07 10:50:06 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Dictionary from "./Dictionary";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer">
+        <small> Coded by Naznin Sultana</small>
+      </footer>
+    </div>
+  );
+}
---
+++ Dictionary.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import axios from "axios";
+import Results from "./Results";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
+
+  function handleResponse(response) {
+    console.log(response.data[0]);
+    setResults(response.data[0]);
+  }
+
+  function search(event) {
+    event.preventDefault();
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    console.log(apiUrl);
+    axios.get(apiUrl).then(handleResponse);
+  }
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input
+          type="search"
+          value={keyword}
+          onChange={handleKeywordChange}
+          placeholder="Enter a word"
+        />
+        <button type="submit">Search</button>
+      </form>
+      {results && <Results results={results} />}
+    </div>
+  );
+}
