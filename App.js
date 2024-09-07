01464efc8c469e3b0d797025b8a5929ea409a983
naznin07 <naffect1@gmail.com>
2024-09-07 11:14:37 -0400

    Update App.js

--- App.js
+++ App.js
@@ -8,8 +8,31 @@ export default function App() {
       <main>
         <Dictionary />
       </main>
-      <footer className="App-footer">
-        <small> Coded by Naznin Sultana</small>
+       <footer className="App-footer">
+        This project was coded by{" "}
+        <a
+          href="https://github.com/naznin07"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Naznin Sultana
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/naznin07/Dictionary"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          open-sourced on GitHub
+        </a>{" "}
+        and{" "}
+        <a
+          href="https://dictionary10.netlify.app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          hosted on Netlify
+        </a>
       </footer>
     </div>
   );
